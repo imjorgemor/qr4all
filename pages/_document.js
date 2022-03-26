d9a339926a17a9ed4d1be399c4e0db1b288cd59d
Jorge Mor <imgeorgemore@icloud.com>
2022-03-26 17:56:33 +0100

    Update documentjs crossOrigin property

--- pages/_document.js
+++ pages/_document.js
@@ -20,7 +20,7 @@ class MyDocument extends Document {
                     <link
                         rel="preconnect" href="https://fonts.googleapis.com"
                     />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                     <link
                         href="https://fonts.googleapis.com/css2?family=Chicle&display=swap" rel="stylesheet"
                     />
